66b6298a39129f64be1a17706762332199bc1500
Hamiriza Firdhan Ardyasta <hamirizafirdhanardyasta@itsss-MacBook-Pro.local>
2020-11-27 17:48:38 +0800

    changed

---
+++ src/components/BreakInterval.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function BreakInterval(props) {
+  function decreaseCounter() {
+    if (props.breakInterval === 1) {
+      return;
+    }
+    props.decreaseBreak();
+  }
+
+  function increaseCounter() {
+    if (props.breakInterval === 60) {
+      return;
+    }
+
+    props.increaseBreak();
+  }
+
+  return (
+    <section>
+      <h4 id="break-label">Break Length</h4>
+      <section className="interval-container">
+        <button
+          disabled={props.isPlay === true ? "disabled" : ""}
+          onClick={decreaseCounter}
+          id="break-decrement"
+        >
+          Down
+        </button>
+        <p className="interval-length" id="break-length">
+          {props.breakInterval}
+        </p>
+        <button
+          disabled={props.isPlay === true ? "disabled" : ""}
+          onClick={increaseCounter}
+          id="break-increment"
+        >
+          Up
+        </button>
+      </section>
+    </section>
+  );
+}
+
+export default BreakInterval;
